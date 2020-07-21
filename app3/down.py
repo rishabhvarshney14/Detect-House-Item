acee63c2a4813f53e17aad8481bbb732d8dfdcaa
rishabhvarshney14 <53183977+rishabhvarshney14@users.noreply.github.com>
2020-07-21 15:10:34 +0530

    Add files via upload

---
+++ app3/down.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='1YHtjrRJXJsGRCfgdkQOdQHIl4rwgBlUx',
+                                    dest_path='./model/model.h5',
+                                    unzip=False)
